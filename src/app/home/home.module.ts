a0959bedfb822a00564f5acb92ea912accc793dc
was.li <wasuwat.se@gmail.com>
2018-12-29 14:22:20 +0700

    add demo state management

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
-import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home/home.component';
+import { HomeRoutingModule } from './home-routing.module'
+import { HomeComponent } from './home/home.component'
+
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  imports: [CommonModule, MatButtonModule, MatCardModule, HomeRoutingModule],
 })
-export class HomeModule { }
+export class HomeModule {}
--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core'
+import { Store, select } from '@ngrx/store'
+import { Observable } from 'rxjs'
+import { Increment, Decrement, Reset } from '../../counter.actions'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  count$: Observable<number>
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.pipe(select('count'))
   }
 
+  increment() {
+    this.store.dispatch(new Increment())
+  }
+
+  decrement() {
+    this.store.dispatch(new Decrement())
+  }
+
+  reset() {
+    this.store.dispatch(new Reset())
+  }
 }
