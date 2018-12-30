f2392c226f120f27c02cf1b52594354fcf8dc5e1
was.li <wasuwat.se@gmail.com>
2018-12-30 18:18:38 +0700

    add employee sub routes

--- src/app/employee/employee-routing.module.ts
+++ src/app/employee/employee-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+import { EmployeeFormComponent } from './employee-form/employee-form.component'
+import { EmployeeListComponent } from './employee-list/employee-list.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: EmployeeListComponent,
+  },
+  {
+    path: 'form',
+    component: EmployeeFormComponent,
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class EmployeeRoutingModule { }
+export class EmployeeRoutingModule {}
