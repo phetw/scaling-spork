c8438a6ab2077f30cc8739d7c767b9a7ecc1002d
was.li <wasuwat.se@gmail.com>
2018-12-30 15:58:44 +0700

    add employee api link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  endpoint: 'https://rocky-brushlands-79767.herokuapp.com',
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  endpoint: 'https://rocky-brushlands-79767.herokuapp.com',
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
