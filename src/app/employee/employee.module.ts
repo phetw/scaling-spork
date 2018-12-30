bc417e9abcff0f32bbbc06cf47ea8c417b446f88
was.li <wasuwat.se@gmail.com>
2018-12-30 16:00:46 +0700

    add employee module

---
+++ src/app/employee/employee.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EmployeeRoutingModule } from './employee-routing.module';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeFormComponent } from './employee-form/employee-form.component';
+
+@NgModule({
+  declarations: [EmployeeListComponent, EmployeeFormComponent],
+  imports: [
+    CommonModule,
+    EmployeeRoutingModule
+  ]
+})
+export class EmployeeModule { }
