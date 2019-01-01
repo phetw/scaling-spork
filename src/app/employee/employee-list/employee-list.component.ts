5296681151d6c869340f473277a4faeed103b242
was.li <wasuwat.se@gmail.com>
2019-01-01 17:19:33 +0700

    integrate api with employee comps

--- src/app/employee/employee-list/employee-list.component.ts
+++ src/app/employee/employee-list/employee-list.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { EmployeeService } from '@app/core/service/employee.service'
+import { Employee } from '@app/store/employee/employee.model'
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
-  styleUrls: ['./employee-list.component.scss']
+  styleUrls: ['./employee-list.component.scss'],
 })
-export class EmployeeListComponent implements OnInit {
+export class EmployeeListComponent implements OnInit, OnDestroy {
+  employees$: Array<Employee>
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService) {}
 
   ngOnInit() {
+    this.getEmployeeData()
   }
 
+  getEmployeeData() {
+    this.employeeService.getEmployees().subscribe(result => {
+      this.employees$ = result
+    })
+  }
+
+  ngOnDestroy() {}
 }
--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -1,15 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
-import { EmployeeRoutingModule } from './employee-routing.module';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
-import { EmployeeFormComponent } from './employee-form/employee-form.component';
+import { EmployeeRoutingModule } from './employee-routing.module'
+import { EmployeeListComponent } from './employee-list/employee-list.component'
+import { EmployeeFormComponent } from './employee-form/employee-form.component'
 
+import { MatCardModule } from '@angular/material/card'
 @NgModule({
   declarations: [EmployeeListComponent, EmployeeFormComponent],
-  imports: [
-    CommonModule,
-    EmployeeRoutingModule
-  ]
+  imports: [CommonModule, EmployeeRoutingModule, MatCardModule],
 })
-export class EmployeeModule { }
+export class EmployeeModule {}
